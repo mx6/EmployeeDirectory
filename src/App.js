abf786ee7ab1577fed91a329748ec910d963434c
M. Adamson <MX@Matthews-MBP.lan>
2020-11-23 15:00:00 -0800

    ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Table from "./components/Table";
+import API from "./utils/API";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h3>Employee Directory</h3>
+       <h5>Click on 'name' to sort alphabetically, or use the search box to find an employee.</h5>
       </header>
+      <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td colSpan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </Table>
+      
     </div>
   );
+
+  // componentDidMount() {
+  //   this.peopleDir("");
+  // }
+  // peopleDir = query => {
+  //   API.search(query)
+  //     .then(res => this.setState({ result: res.data }))
+  //     .catch(err => console.log(err));
+  // };
+
+  // handleFormSubmit = event => {
+  //   event.preventDefault();
+  //   this.peopleDir(this.state.search);
+  // };
+
 }
 
 export default App;
---
+++ src/components/Table.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Table(props) {
+    return (
+        <div className="Table">
+            <Table>
+            </Table>
+        </div>
+    );
+}
+
+export default Table;
