9f1cf2e3f5582a36cc73a091a45a2111b483af1b
M. Adamson <MX@Matthews-MBP.lan>
2020-12-21 07:45:17 -0800

    styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ function Header() {
     return (
         <div>
             <header className="header">
-                <h3>Employee Directory</h3>
-                <h5>Click on 'Last Name' to sort alphabetically, or use the search box to find an employee.</h5>
+                <h1>Employee Directory</h1>
+                <h4>Click on 'Last Name' to sort alphabetically, or use the search box to find an employee.</h4>
             </header>
 
         </div>
